7b8b1711d8dcdd3e4486d2568293575187d3e6dd
Sinan Kaya <skaya.sinan@outlook.com>
2025-09-05 15:15:12 +0300

    Add project files.

---
+++ EmailApp/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EmailApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EmailApp.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppUser>().HasMany(message => message.SentMessages).WithOne(s=>s.Sender).HasForeignKey(x => x.SenderId).OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<AppUser>().HasMany(message => message.RecieverMessages).WithOne(y=>y.Reciever).HasForeignKey(x => x.RecieverId).OnDelete(:DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ EmailApp/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmailApp.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<Message> SentMessages { get; set; }
+        public ICollection<Message> RecieverMessages { get; set; }
+        
+    }
+}
